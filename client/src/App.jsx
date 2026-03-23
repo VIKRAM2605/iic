ec00501a91eb77596f1ee3e0b2a9802056f307f8
yalinisrit167 <yalinisrit167@gmail.com>
2026-03-23 11:38:25 +0530

    login.jsx added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Applayout from "./applayout/Applayout";
 import EventDetails from "./pages/admin/EventDetails";
+import Login from "./pages/admin/Login";
 
 function App() {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
       <Route element={<Applayout />}>
         <Route path="/eventdetails" element={<EventDetails />} />
         <Route path="*" element={<Navigate to="/eventdetails" replace />} />
